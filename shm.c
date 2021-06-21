9ff5ba5f922cef49d9bd4ebd28f6a00fed46c6fe
giang <giang.to.7737@gmail.com>
2021-06-21 11:02:29 -0700

    change

--- shm.c
+++ shm.c
@@ -32,8 +32,38 @@ int shm_open(int id, char **pointer) {
 
 //you write this
 
+acquire(&(shm_table.lock));
+int found = 0;
+int i; // what the heck is C99 mode
+struct proc *curproc = myproc();
+int sz = PGROUNDUP(curproc->sz);
 
-
+for(i = 0;i<64;i++){
+    if(shm_table.shm_pages[i].id == id ){
+        found = 1;
+        mappages(curproc->pgdir,(char*)sz,PGSIZE,V2P(shm_table.shm_pages[i].frame),PTE_W|PTE_U);
+        shm_table.shm_pages[i].refcnt++;
+        *pointer= (char*)sz;
+        curproc->sz = sz+PGSIZE;
+        break;
+    }
+}
+if(!found){
+    for(i=0;i<64;i++){
+        if(shm_table.shm_pages[i].id ==0){
+            shm_table.shm_pages[i].id = id;
+            shm_table.shm_pages[i].frame = kalloc();
+            shm_table.shm_pages[i].refcnt=1;
+            memset(shm_table.shm_pages[i].frame,0,PGSIZE); //set up memory with physical address
+            mappages(curproc->pgdir,(char *)sz,PGSIZE,V2P(shm_table.shm_pages[i].frame),PTE_W|PTE_U);
+            shm_table.shm_pages[i].refcnt++;
+            *pointer= (char*)sz;
+            curproc->sz = sz+PGSIZE;
+            break;
+        }
+    }
+}
+release(&(shm_table.lock));
 
 return 0; //added to remove compiler warning -- you should decide what to return
 }
@@ -41,8 +71,24 @@ return 0; //added to remove compiler warning -- you should decide what to return
 
 int shm_close(int id) {
 //you write this too!
-
-
+acquire(&(shm_table.lock));
+int i; //because for some reason it is only allowed in C99 mode
+for(i =0;i<64;i++){
+    if(shm_table.shm_pages[i].id==id && shm_table.shm_pages[i].refcnt > 0){
+        shm_table.shm_pages[i].refcnt--;
+        break;
+    }
+    else if(shm_table.shm_pages[i].id==id && shm_table.shm_pages[i].refcnt == 0){
+        shm_table.shm_pages[i].id = 0;
+        shm_table.shm_pages[i].frame = 0;
+        break;
+    }
+    else { // not found id
+        release(&(shm_table.lock));
+        break;
+    }
+}
+release(&(shm_table.lock));
 
 
 return 0; //added to remove compiler warning -- you should decide what to return
